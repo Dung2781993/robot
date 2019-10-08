86e0cff2e1f8831f16d697c5a82f1a1e55e44da1
Dung2781993@gmail.com <Dung2781993@gmail.com>
2019-10-08 15:30:21 +1100

    Update move, place , left and right method for robot
    Update check valid move

--- ToyRobot/Program.cs
+++ ToyRobot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToyRobot.Model;
 
 namespace ToyRobot
 {
@@ -11,22 +12,23 @@ namespace ToyRobot
         static void Main(string[] args)
         {
             var command = " ";
-
+            var movement = new Movement(5, 5);
             Console.WriteLine("Enter command to control robot: (Type Exit to stop program)");
-
-            while (true){
-                
+            Console.WriteLine("Please enter command: ");
+            while (true){           
                 //Get user input
-                Console.WriteLine("Please enter command: ");
-                command = Console.ReadLine();
                 
-                if(command.ToUpper() == "EXIT")
+                command = Console.ReadLine();
+                var result = movement.execute(command);
+                if (command.ToUpper() == "EXIT")
                 {
                     break;
                 }
-
-
-                Console.WriteLine();
+                if(result != "")
+                {
+                    Console.WriteLine(result);
+                }
+                
             }
 
             Console.WriteLine("Thank you for your participation");
