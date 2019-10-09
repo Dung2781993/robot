5d87b4ab0bbf1e811a13c2d32c813b68540e1c17
Dung2781993@gmail.com <Dung2781993@gmail.com>
2019-10-09 16:28:07 +1100

    Create Unit testing for project

--- ToyRobotTests/UnitTest.cs
+++ ToyRobotTests/UnitTest.cs
@@ -8,7 +8,7 @@ namespace ToyRobotTests
     public class UnitTest
     {
         [TestMethod]
-        public void Robot_WhenNotPlacedYet()
+        public void Robot_TEST_WhenNotPlacedYet()
         {
             //arrange
             var movement = new Movement(5,5);
@@ -19,7 +19,7 @@ namespace ToyRobotTests
         }
 
         [TestMethod]
-        public void Robot_AfterBeingPlaced()
+        public void Robot_TEST_AfterBeingPlaced()
         {
             //arrange
             var movement = new Movement(5, 5);
@@ -30,7 +30,7 @@ namespace ToyRobotTests
         }
 
         [TestMethod]
-        public void Robot_Report_AfterBeingPlaced()
+        public void Robot_TEST_Report_AfterBeingPlaced()
         {
             //arrange
             var movement = new Movement(5, 5);
@@ -42,7 +42,7 @@ namespace ToyRobotTests
         }
 
         [TestMethod]
-        public void Robot_Report_0_1_N_AfterPlaced_0_0_N_AndSingleMove()
+        public void Robot_TEST_CASE_A()
         {
             //arrange
             var movement = new Movement(5, 5);
@@ -54,5 +54,82 @@ namespace ToyRobotTests
             Assert.AreEqual("0,1,NORTH", result);
         }
 
+        [TestMethod]
+        public void Robot_TEST_CASE_B()
+        {
+            //arrange
+            var movement = new Movement(5, 5);
+            //act
+            var result = movement.execute("PLACE 0,0,NORTH");
+            result = movement.execute("LEFT");
+            result = movement.execute("REPORT");
+            //assert
+            Assert.AreEqual("0,0,WEST", result);
+        }
+
+
+        [TestMethod]
+        public void Robot_TEST_CASE_C()
+        {
+            //arrange
+            var movement = new Movement(5, 5);
+            //act
+            var result = movement.execute("PLACE 1,2,EAST");
+            result = movement.execute("MOVE");
+            result = movement.execute("MOVE");
+            result = movement.execute("LEFT");
+            result = movement.execute("MOVE");
+            result = movement.execute("REPORT");
+            //assert
+            Assert.AreEqual("3,3,NORTH", result);
+        }
+
+
+        //Invalid place - out of bound 
+        [TestMethod]
+        public void Robot_TEST_PLACE_OUT_OF_BOUND()
+        {
+            //arrange
+            var movement = new Movement(5, 5);
+            //act
+            var result = movement.execute("PLACE 0,10,NORTH");
+            //assert
+            Assert.AreEqual(Movement.OUT_OF_TABLE, result);
+        }
+
+        [TestMethod]
+        public void Robot_TEST_MOVE_OUT_OF_BOUND()
+        {
+            //arrange
+            var movement = new Movement(5, 5);
+            //act
+            var result = movement.execute("PLACE 4,5,NORTH");
+            result = movement.execute("MOVE");
+            //assert
+            Assert.AreEqual(Movement.OUT_OF_TABLE, result);
+        }
+
+        [TestMethod]
+        public void Robot_TEST_INVALID_FORMAT_PLACE()
+        {
+            //arrange
+            var movement = new Movement(5, 5);
+            //act
+            var result = movement.execute("PLACE 4,NORTH");
+            //assert
+            Assert.AreEqual(Movement.INVALID_FORMAT_PLACE, result);
+        }
+
+
+        [TestMethod]
+        public void Robot_TEST_INVALID_FORMAT_PLACE_SYNTAX()
+        {
+            //arrange
+            var movement = new Movement(5, 5);
+            //act
+            var result = movement.execute("PLACE 4,4,NORTHERN");
+            //assert
+            Assert.AreEqual(Movement.INVALID_FORMAT_PLACE, result);
+        }
     }
 }
